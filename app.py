be01adb9f1e70ce1e413ebb07695a1d6669a744d
raulherale <51846188+raulherale@users.noreply.github.com>
2022-08-12 17:17:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import dash
+from jupyter_dash import JupyterDash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
