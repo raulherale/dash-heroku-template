7e2b7b53b2e35bd3ab78bc89b99588a9c1ac1451
raulherale <51846188+raulherale@users.noreply.github.com>
2022-08-12 17:25:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,14 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import dash
-from jupyter_dash import JupyterDash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(
-    [
+app.layout = html.Div([
         html.H1("Gender Wage Gap study using \n the 2019 General Social Survey")
     ])
 
